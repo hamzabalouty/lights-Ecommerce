8adb4eb508b53e393aa296db9624733a9064f3be
hamzabalouty <123960068+hamzabalouty@users.noreply.github.com>
2023-11-13 21:47:34 +0200

    Update Account.js

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Account.css";
 
 const Account = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="account">
       <div className="account-title">
