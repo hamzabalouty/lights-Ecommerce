7406cf0795e0595642d60f9f645bf71fe75386b9
hamzabalouty <123960068+hamzabalouty@users.noreply.github.com>
2023-11-13 21:53:31 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -231,3 +231,48 @@ export const shopData = [
     size: "M"
   }
 ];
+
+export const collectionData = [
+  {
+    id: 1,
+    img:
+      "https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0022_Layer_10_a8deedac-1637-425d-8575-4b63d8b7bbce_600x.jpg?v=1535536158",
+    title: "Beautiful Art Decor Light",
+    price: "510.00"
+  },
+  {
+    id: 2,
+    img:
+      "https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0011_Layer_23_d62b2ddf-017c-4b57-a240-135bb9ee607a_600x.jpg?v=1535355702",
+    title: "colour paper lamp",
+    price: "319.00"
+  },
+  {
+    id: 3,
+    img:
+      "https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0005_Layer_27_34d03e1f-09d8-4bab-90ca-d273f49ab1a5_600x.jpg?v=1535353637",
+    title: "1274 5-Watt Table Lamp",
+    price: "540.00"
+  },
+  {
+    id: 4,
+    img:
+      "https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0026_Layer_6_170aca74-d7da-4416-a015-d77dc66dd067_600x.jpg?v=1535352653",
+    title: "Decor Elegant Light",
+    price: "142.00"
+  },
+  {
+    id: 5,
+    img:
+      "https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0027_Layer_5_600x.jpg?v=1535352210",
+    title: "220-Watt Floor Lamp",
+    price: "210.00"
+  },
+  {
+    id: 6,
+    img:
+      "https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0006_shutterstock_771815044_600x.jpg?v=1535353936",
+    title: "Brown Wood Table Lamp",
+    price: "750.00"
+  }
+];
