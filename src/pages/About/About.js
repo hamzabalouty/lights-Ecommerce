19b75ee8130bb096230ac4bf53f5b3a56ce57c6a
hamzabalouty <123960068+hamzabalouty@users.noreply.github.com>
2023-11-13 21:47:03 +0200

    Update About.js

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./About.css";
 
 const About = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="about">
       <div className="about-title">
