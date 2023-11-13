a33f58b71370f1f76f232c5e451ac246ee48f777
hamzabalouty <123960068+hamzabalouty@users.noreply.github.com>
2023-11-13 21:45:28 +0200

    Update Collection.js

--- src/components/Collection/Collection.js
+++ src/components/Collection/Collection.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "./Collection.css";
+import { collectionData } from "../../data";
 import { MdOutlineFavoriteBorder } from "react-icons/md";
 import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { addToCart } from "../../rtk/slices/cart-slice";
+import { addCart } from "../../rtk/slices/favorite-slice";
+import { useDispatch, useSelector } from "react-redux";
 
 const Collection = () => {
+  const dispatch = useDispatch();
+
   return (
     <div className="collections">
       <div className="collections-title">
@@ -16,45 +23,27 @@ const Collection = () => {
           <button>Best Sellers</button>
         </div>
         <div className="collection-box-container">
-          <div className="collection-box">
-            <img src="https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0011_Layer_23_d62b2ddf-017c-4b57-a240-135bb9ee607a_600x.jpg?v=1535355702" />
-            <div className="collection-box-btn">
-              <button>
-                <MdOutlineFavoriteBorder className="favorite-icon" />
-              </button>
-              <button>
-                <AiOutlineSearch className="favorite-icon" />
-              </button>
+          {collectionData.map((item) => (
+            <div className="collection-box">
+              <img src={item.img} alt="collection-img" />
+              <div className="collection-box-btn">
+                <button>
+                  <MdOutlineFavoriteBorder
+                    onClick={() => dispatch(addCart(item))}
+                    className="favorite-icon"
+                  />
+                </button>
+                <button>
+                  <AiOutlineShoppingCart
+                    onClick={() => dispatch(addToCart(item))}
+                    className="cart-icon"
+                  />
+                </button>
+              </div>
+              <p>{item.title}</p>
+              <h5>${item.price}</h5>
             </div>
-            <p>color paper lamp</p>
-            <h5>$319.00</h5>
-          </div>
-          <div className="collection-box">
-            <img src="https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0005_Layer_27_34d03e1f-09d8-4bab-90ca-d273f49ab1a5_600x.jpg?v=1535353637" />
-            <div className="collection-box-btn">
-              <button>
-                <MdOutlineFavoriteBorder className="favorite-icon" />
-              </button>
-              <button>
-                <AiOutlineSearch className="favorite-icon" />
-              </button>
-            </div>
-            <p>color paper lamp</p>
-            <h5>$319.00</h5>
-          </div>
-          <div className="collection-box">
-            <img src="https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0026_Layer_6_170aca74-d7da-4416-a015-d77dc66dd067_600x.jpg?v=1535352653" />
-            <div className="collection-box-btn">
-              <button>
-                <MdOutlineFavoriteBorder className="favorite-icon" />
-              </button>
-              <button>
-                <AiOutlineSearch className="favorite-icon" />
-              </button>
-            </div>
-            <p>color paper lamp</p>
-            <h5>$319.00</h5>
-          </div>
+          ))}
         </div>
       </div>
     </div>
