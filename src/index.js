877b3012904d2ff7b5b087f3617947bc74f4530d
hamzabalouty <123960068+hamzabalouty@users.noreply.github.com>
2023-11-13 21:53:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
+import { store } from "./rtk/store";
+import { Provider } from "react-redux";
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
