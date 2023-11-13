860cd76ac808485bc0d8280bbfe760f2350e1cbe
hamzabalouty <123960068+hamzabalouty@users.noreply.github.com>
2023-11-13 23:27:20 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -238,41 +238,131 @@ export const collectionData = [
     img:
       "https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0022_Layer_10_a8deedac-1637-425d-8575-4b63d8b7bbce_600x.jpg?v=1535536158",
     title: "Beautiful Art Decor Light",
-    price: "510.00"
+    price: "510.00",
+    category: 'Featured Products'
   },
   {
     id: 2,
     img:
       "https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0011_Layer_23_d62b2ddf-017c-4b57-a240-135bb9ee607a_600x.jpg?v=1535355702",
     title: "colour paper lamp",
-    price: "319.00"
+    price: "319.00",
+    category: 'Featured Products'
   },
   {
     id: 3,
     img:
       "https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0005_Layer_27_34d03e1f-09d8-4bab-90ca-d273f49ab1a5_600x.jpg?v=1535353637",
     title: "1274 5-Watt Table Lamp",
-    price: "540.00"
+    price: "540.00",
+    category: 'Featured Products'
   },
   {
     id: 4,
     img:
       "https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0026_Layer_6_170aca74-d7da-4416-a015-d77dc66dd067_600x.jpg?v=1535352653",
     title: "Decor Elegant Light",
-    price: "142.00"
+    price: "142.00",
+    category: 'Featured Products'
   },
   {
     id: 5,
     img:
       "https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0027_Layer_5_600x.jpg?v=1535352210",
     title: "220-Watt Floor Lamp",
-    price: "210.00"
+    price: "210.00",
+    category: 'Featured Products'
   },
   {
     id: 6,
     img:
       "https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0006_shutterstock_771815044_600x.jpg?v=1535353936",
     title: "Brown Wood Table Lamp",
-    price: "750.00"
-  }
+    price: "750.00",
+    category: 'Featured Products'
+  },
+  {
+    id: 7,
+    img: 'https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0021_Layer_11_48f998c3-4ffd-42ed-a06d-c7c60021359b_600x.jpg?v=1535359765',
+    title: 'Decor Gorgeous Light',
+    price: '510.00',
+    category: 'New Products'
+  },
+  {
+    id: 8,
+    img: 'https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0014_Layer_18_b75dac0c-5704-42c2-979b-13499f97fb0f_600x.jpg?v=1535356772',
+    title: 'Decorative',
+    price: '240.00',
+    category: 'New Products'
+  },
+  {
+    id: 9,
+    img: 'https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0006_shutterstock_771815044_479a51ff-e14d-45ec-a724-84de73f5a162_600x.jpg?v=1535354154',
+    title: 'Elegant Light For Home',
+    price: '750.00',
+    category: 'New Products'
+  },
+  {
+    id: 10,
+    img: 'https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0030_Layer_2_600x.jpg?v=1535352005',
+    title: 'Decorative Standing Light',
+    price: '389.00',
+    category: 'New Products'
+  },
+  {
+    id: 11,
+    img: 'https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0021_Layer_11_600x.jpg?v=1535357512',
+    title: 'Decorative Bedside Table',
+    price: '510.00',
+    category: 'New Products'
+  },
+  {
+    id: 12,
+    img: 'https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0012_Layer_22_600x.jpg?v=1535355906',
+    title: 'Fancy Handing Lights',
+    price: '219.00',
+    category: 'New Products'
+  },
+  {
+    id: 13,
+    img: 'https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0019_Layer_13_8c6d831a-2f68-4ec0-b612-9a335bdeab00_600x.jpg?v=1535357255',
+    title: 'Mesh Hanging Pendant',
+    price: '319.00',
+    category: 'Best Sellers'
+  },
+  {
+    id: 14,
+    img: 'https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0010_Layer_24_52e83659-397d-4a76-a21f-466b09f52fee_600x.jpg?v=1535355511',
+    title: 'Lamp Modern Light',
+    price: '750.00',
+    category: 'Best Sellers'
+  },
+  {
+    id: 15,
+    img: 'https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0007_Layer_26_4a57cf15-59b3-4a81-8e39-dd7b186a3fb3_600x.jpg?v=1535354287',
+    title: 'Hanging Ceiling Light',
+    price: '329.00',
+    category: 'Best Sellers'
+  },
+  {
+    id: 16,
+    img: 'https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0000_Layer_31_d65eb67d-3973-4763-8cce-f15979fbf956_600x.jpg?v=1535353162',
+    title: 'Living Room Lamp',
+    price: '389.00',
+    category: 'Best Sellers'
+  },
+  {
+    id: 17,
+    img: 'https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0017_Layer_15_bb5a6054-30f9-4115-8c44-e55482d51256_600x.jpg?v=1535352423',
+    title: 'Foldable Table Lamp',
+    price: '540.00',
+    category: 'Best Sellers'
+  },
+  {
+    id: 18,
+    img: 'https://dt-lights.myshopify.com/cdn/shop/products/interior-products_0003_shutterstock_715229632_600x.jpg?v=1535353408',
+    title: 'Living Hanging Light',
+    price: '329.00',
+    category: 'Best Sellers'
+  },
 ];
